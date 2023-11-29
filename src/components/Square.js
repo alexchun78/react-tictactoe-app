0e6909826efbd8ce51d1690b1e04a3e43f56faaa
alexchun <alexchun78@gmail.com>
2023-11-29 16:54:05 +0900

    done tictactoe basic version

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import './Square.css';
 
-export default class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value:null,
-    };
-  }
-
-  onClickEvent(e){
-    this.setState({value:'X'}); 
-    console.log(this.state.value);
-  }
-
-  render() {
-    return (
-      <button className="square" onClick={(e)=>this.onClickEvent(e)}>
-        {this.state.value}
+const Square = ({value, onClick})=>{
+  return (
+    <>
+      <button className="square" onClick={onClick}>
+        {value}
       </button>
-    ) 
-  }
-}
+    </>
+  );
+};
+
+export default Square;
